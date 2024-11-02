c57c9eda4d6df01aa4a0991c752b5d917a087a65
Scott Green <skevingreen@gmail.com>
2024-11-02 11:45:06 -0700

    completed assignment 2.2

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,23 +3,50 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Scott Green
+  Date: October 28, 2024
+  Filename: script.js
 */
 
 "use strict";
 
+// Function to create a character based on provided parameters.
 function createCharacter(name, gender, characterClass) {
-  // TODO: Implement this function
+  return {
+    getName: function() {
+      return name;
+    },
+    getGender: function() {
+      return gender;
+    },
+    getClass: function() {
+      return characterClass;
+    }
+  }
 }
 
+// Create the character with specified details and display the results.
 document.getElementById("generateHero").addEventListener("click", function(e) {
   e.preventDefault();
 
-  // TODO: Get form values
+  let name;
+  let gender;
+  let characterClass;
+  let character;
+  let output;
 
-  // TODO: Create character
+  // Get form values
+  name = document.getElementById("heroName").value;
+  gender = document.getElementById("heroGender").value;
+  characterClass = document.getElementById("heroClass").value;
 
-  // TODO: Display character information
+  // Create character
+  character = createCharacter(name, gender, characterClass);
+
+  // Display character information
+  output = document.getElementById("characterOutput");
+  output.innerHTML = `<br><p>Character Information</p>
+                      <p>Name: <span id="shadow">${character.getName()}</span></p>
+                      <p>Gender: <span id="shadow">${character.getGender()}</span></p>
+                      <p>Class: <span id="shadow">${character.getClass()}</span></p>`;
 });
